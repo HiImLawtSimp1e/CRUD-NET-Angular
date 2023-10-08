549b5def03dd26a217ecca4ee904361876228ea7
My PC <My PC@LAPTOP-TJU16KH0>
2023-10-08 09:53:25 +0700

    Push Product

---
+++ SuperHero-API/SuperHero-API/Context/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperHero_API.Domain;
+
+namespace SuperHero_API.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+            
+        }
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
